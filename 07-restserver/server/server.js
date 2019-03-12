8f68089d56a20e1f9f939dcd0e56956aad2a79c0
Juan Navas <juan.navas@eyss-dev.com>
2019-03-12 14:50:55 -0400

    update

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,8 +1,36 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
  
-app.get('/', function (req, res) {
-  res.json('Hello World')
+// parse application/json
+app.use(bodyParser.json())
+ 
+ 
+app.get('/usuario', function (req, res) {
+  res.json('get usuario')
+})
+
+app.post('/usuario', function (req, res) {
+
+  let body = req.body
+
+  res.json('post usuario')
+})
+
+app.put('/usuario/:id', function (req, res) {
+
+  let id = req.params.id;
+
+  res.json({
+    id
+  })
+})
+
+app.delete('/usuario', function (req, res) {
+  res.json('delete usuario')
 })
 
 app.listen(3000, () => {
